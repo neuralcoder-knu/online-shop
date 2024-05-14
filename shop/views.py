dc63f1c68401d2b687735bdd84e049c73113cdc6
Yehor Vitvytskyi <grey5393@gmail.com>
2024-05-14 23:15:40 +0300

    created config and configures views rendering

---
+++ shop/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render, get_object_or_404
+from shop.models import Product, Category
+
+
+def home(request, slug=None):
+    products = Product.objects.filter(status=True)
+    categories = Category.objects.filter(is_sub=False)
+    if slug:
+        category = get_object_or_404(Category, slug=slug)
+        products = products.filter(category=category)
+    return render(request, 'shop/home.html', {'products': products, 'categories': categories})
+
+
+def product_detail(request, slug):
+    product = get_object_or_404(Product, slug=slug)
+    # TODO CART form = Add2CartForm()
+    return render(request, 'shop/product_detail.html', {'product': product})  # TODO , 'form': form
