59783369457f751aad4ccf5cf481a292d3434fa3
Николай Коваль <mykolai.koval@nks.com.ua>
2024-05-24 07:02:43 +0300

    orders init

---
+++ orders/admin.py
@@ -0,0 +1,21 @@
+from django.contrib import admin
+
+from orders.models import Order, OrderItem, Coupon
+
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+    raw_id_fields = ('product',)
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'created', 'status')
+    list_filter = ('status',)
+    inlines = (OrderItemInline,)
+
+
+@admin.register(Coupon)
+class CouponAdmin(admin.ModelAdmin):
+    list_display = ('code', 'valid_from', 'valid_to', 'discount', 'status')
+    list_filter = ('status', 'valid_from', 'valid_to')
+    search_fields = ('code',)
